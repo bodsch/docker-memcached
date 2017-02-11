77028f615bb9e7e167e9280f5d4093d53d309a73
Bodo Schulz <support@xanhaem.de>
2017-02-11 11:02:49 +0100

    strip down, rebuild and update README.md

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
 
-FROM bodsch/docker-alpine-base:1701-02
+FROM alpine:latest
 
 MAINTAINER Bodo Schulz <bodo@boone-schulz.de>
 
-LABEL version="1.2.0"
+LABEL version="1702-02"
+
+ENV \
+  ALPINE_MIRROR="dl-cdn.alpinelinux.org" \
+  ALPINE_VERSION="v3.5" \
+  TERM=xterm
 
 EXPOSE 11211
 
 # ---------------------------------------------------------------------------------------
 
 RUN \
+  echo "http://${ALPINE_MIRROR}/alpine/${ALPINE_VERSION}/main"       > /etc/apk/repositories && \
+  echo "http://${ALPINE_MIRROR}/alpine/${ALPINE_VERSION}/community" >> /etc/apk/repositories && \
   apk --no-cache update && \
   apk --no-cache upgrade && \
   apk --no-cache add \
@@ -18,7 +25,9 @@ RUN \
     bash \
     nano \
     tree \
-    curl && \
+    ca-certificates \
+    curl \
+    supervisor && \
   rm -rf \
     /tmp/* \
     /var/cache/apk/*
--- rootfs/opt/startup.sh
+++ rootfs/opt/startup.sh
@@ -1,48 +1,8 @@
 #!/bin/sh
 #
 
-
-if [ ${DEBUG} ]
-then
-  set -x
-fi
-
 MEMCACHED_MEMORY=${MEMCACHED_MEMORY:-8}
 
-# -------------------------------------------------------------------------------------------------
-
-cfgFile="/etc/supervisor.d/memcached.ini"
-
-createConfig() {
-
-  sed -i \
-    -e "s/%MEMORY%/${MEMCACHED_MEMORY}/" \
-    ${cfgFile}
-}
-
-
-startSupervisor() {
-
-  echo -e "\n Starting Supervisor.\n\n"
-
-  if [ -f /etc/supervisord.conf ]
-  then
-    /usr/bin/supervisord -c /etc/supervisord.conf >> /dev/null
-  fi
-}
-
-# -------------------------------------------------------------------------------------------------
-
-run() {
-
-  createConfig
-
-  /usr/bin/memcached -l 0.0.0.0 -m ${MEMCACHED_MEMORY} -u memcached
-
-#  startSupervisor
-
-}
-
-run
+/usr/bin/memcached -l 0.0.0.0 -m ${MEMCACHED_MEMORY} -u memcached
 
 # EOF
